1498036975b1447eefb1fd57a998c4b70814a882
Thumpy <KaliRaines@protonmail.com>
2024-11-25 09:28:19 -0700

    re-orded log colors

--- src/logger/term_logger.cpp
+++ src/logger/term_logger.cpp
@@ -18,19 +18,22 @@ void log_to_terminal(const std::string &message, LogLevel level) {
 
 std::string terminal_color_from_level(LogLevel level) {
   std::string color_code = "\033[0m"; // Default
-
-  if ((level & INFO) != 0) {
-    return color_code.append("\033[1m"); // Highlighted
-  } else if ((level & DEBUG) != 0) {
-    return color_code; // Default
-  } else if ((level & WARNING) != 0) {
-    return color_code.append("\033[33m"); // Yellow
-  } else if ((level & ERROR) != 0) {
-    return color_code.append("\033[31m"); // Red
-  } else if ((level & CRITICAL) != 0) {
+  if ((level & CRITICAL) != 0) {
     return color_code.append("\033[41m\033[1m"); // Red highlighted Background
   }
-  return color_code.append("\033[45m"); // Unknown
+  if ((level & ERROR) != 0) {
+    return color_code.append("\033[31m"); // Red
+  }
+  if ((level & WARNING) != 0) {
+    return color_code.append("\033[33m"); // Yellow
+  }
+  if ((level & INFO) != 0) {
+    return color_code.append("\033[1m"); // Highlighted
+  }
+  if ((level & DEBUG) != 0) {
+    return color_code; // Default
+  }
+  return color_code.append("\033[45m"); // None of the above - Purple background
 }
 } // namespace Logger
 } // namespace Core
--- src/logger/term_logger.hpp
+++ src/logger/term_logger.hpp
@@ -7,7 +7,10 @@ namespace Thumpy {
 namespace Core {
 namespace Logger {
 
+/// log message to terminal
 void log_to_terminal(const std::string &message, LogLevel level);
+
+/// get color value string from log level
 std::string terminal_color_from_level(LogLevel level);
 
 } // namespace Logger

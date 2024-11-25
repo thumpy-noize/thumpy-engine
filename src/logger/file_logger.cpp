05411ad167568248986d51e3bdcfb647a5619a6e
Thumpy <KaliRaines@protonmail.com>
2024-11-25 11:08:39 -0700

    Added unit test for logger

--- src/logger/file_logger.cpp
+++ src/logger/file_logger.cpp
@@ -8,32 +8,32 @@ namespace Core {
 namespace Logger {
 
 int file_log_vision = ALL;
-std::ofstream log_file;
-
-std::string log_path = "log.dump";
+std::ofstream *log_file = new std::ofstream();
 
 void start_log_file() {
   // Open log file
-  log_file.open(log_path, std::ofstream::out | std::ofstream::trunc);
-  if (!log_file.is_open()) {
+  log_file->open(log_path, std::ofstream::out | std::ofstream::trunc);
+  if (!log_file->is_open()) {
     log("Log file failed to open.", ERROR);
   }
 }
 
 void close_log_file() {
   // Close file
-  if (log_file.is_open()) {
+  if (log_file->is_open()) {
     log("Dumping log to " + log_path, INFO);
-    log_file.close();
+    log_file->close();
   }
 }
 
 void log_to_file(const std::string &message, LogLevel level) {
   if ((level & file_log_vision) != 0) {
     // Append to file
-    log_file << message << std::endl;
+    *log_file << message << std::endl;
   }
 }
+
+std::ofstream *get_log_file() { return log_file; }
 
 } // namespace Logger
 
--- src/logger/file_logger.hpp
+++ src/logger/file_logger.hpp
@@ -1,14 +1,18 @@
 #pragma once
 
 #include "logger_helper.hpp"
+#include <fstream>
 
 namespace Thumpy {
 namespace Core {
 namespace Logger {
 
+const std::string log_path = "log.dump";
+
 void start_log_file();
 void close_log_file();
 void log_to_file(const std::string &message, LogLevel level);
+std::ofstream *get_log_file();
 
 } // namespace Logger
 

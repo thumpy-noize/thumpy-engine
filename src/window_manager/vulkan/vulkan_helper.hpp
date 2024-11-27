ee491cc70ce3f4cdc0c76b23cea87caf5f901daa
Thumpy <KaliRaines@protonmail.com>
2024-11-26 23:36:42 -0700

    updated helper

--- src/window_manager/vulkan/vulkan_helper.hpp
+++ src/window_manager/vulkan/vulkan_helper.hpp
@@ -2,11 +2,23 @@
 
 #include <cstdint>
 #include <optional>
+#include <vector>
 namespace Thumpy {
 namespace Core {
 namespace Windows {
 namespace Vulkan {
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+
 struct QueueFamilyIndices {
+
   std::optional<uint32_t> graphicsFamily;
   std::optional<uint32_t> presentFamily;
 

c447f4f4514654e467b009b91382b027f2b7eba4
Thumpy <KaliRaines@protonmail.com>
2024-11-29 14:19:45 -0700

    Added comments

--- src/window_manager/vulkan/vulkan_helper.cpp
+++ src/window_manager/vulkan/vulkan_helper.cpp
@@ -70,33 +70,42 @@ uint32_t find_memory_type(VkPhysicalDevice physicalDevice, uint32_t typeFilter,
 
 namespace Shapes {
 
+/**
+ * @brief Creates a sierpinski triangle from list of triangles
+ * shout out to jacob keller for the guidance -
+ * (https://www.vfxkeeler.com/post/vulkan-sierpinski-triangle)
+ * @param recursions number of times to fractal
+ * @param startingTriangle starting triangles
+ * @return std::vector<Vertex> sierpinski triangles
+ */
 std::vector<Vertex>
 generate_sierpinski_triangle(uint32_t recursions,
                              std::vector<Vertex> startingTriangle) {
+
   Logger::log("Generating sierpinski - recursions left: " +
                   std::to_string(recursions) + "",
               Logger::INFO);
 
   std::vector<Vertex> nextTriangle;
 
-  // for each vert in the triangles
+  // Create a new triangle for each vertex
   for (int i = 0; i < startingTriangle.size(); i++) {
 
-    int vertMod = i % 3;
+    int vertMod = i % 3; // Vertex position in triangle
     int nextIndex;
     int lastIndex;
     Vertex lastVertex;
     Vertex nextVertex;
     switch (vertMod) {
-    case 0:
+    case 0: // if A get B & C
       nextIndex = i + 1;
       lastIndex = i + 2;
       break;
-    case 1:
+    case 1: // if B get C & A
       nextIndex = i + 1;
       lastIndex = i - 1;
       break;
-    case 2:
+    case 2: // if C get B & A
       nextIndex = i - 2;
       lastIndex = i - 1;
       break;
@@ -105,21 +114,27 @@ generate_sierpinski_triangle(uint32_t recursions,
       lastIndex = i;
     }
 
-    lastVertex = Vertex::mid(startingTriangle[lastIndex], startingTriangle[i]);
+    // get midpoint between this vertex & next index
     nextVertex = Vertex::mid(startingTriangle[nextIndex], startingTriangle[i]);
+    // get midpoint between this vertex & last index
+    lastVertex = Vertex::mid(startingTriangle[lastIndex], startingTriangle[i]);
 
-    nextTriangle.push_back(nextVertex);
-    nextTriangle.push_back(lastVertex);
-    nextTriangle.push_back(startingTriangle[i]);
+    nextTriangle.push_back(nextVertex);          // midpoint
+    nextTriangle.push_back(lastVertex);          // midpoint
+    nextTriangle.push_back(startingTriangle[i]); // this vertex
   }
 
   recursions--;
-  if (recursions == 0) {
-    return startingTriangle;
+  if (recursions == 0) { // If we have finished the requested recursions
+    // return the last triangle in the chain
+    return nextTriangle;
   }
 
+  // Repeat for each vertex
+  // Returning the results of our new triangle sierpinskied
   return generate_sierpinski_triangle(recursions, nextTriangle);
 }
+
 } // namespace Shapes
 
 #pragma endregion Shape generation

45f7e7749f24e8b5e9a1ba57e5609ec4977e2eae
Thumpy <KaliRaines@protonmail.com>
2024-12-01 16:46:52 -0700

    updated log time to string

--- src/logger/logger_helper.cpp
+++ src/logger/logger_helper.cpp
@@ -1,5 +1,6 @@
 
 #include "logger_helper.hpp"
+
 #include <chrono>
 #include <ctime>
 #include <string>
@@ -8,25 +9,25 @@ namespace Thumpy {
 namespace Core {
 namespace Logger {
 
-std::string format_message(const std::string *message, LogLevel level) {
-  return std::string("[" + log_level_to_string(level) + "] [" +
-                     get_time_as_string() + "]: " + message->c_str());
+std::string format_message( const std::string *message, LogLevel level ) {
+  return std::string( "[" + log_level_to_string( level ) + "] [" +
+                      get_time_as_string() + "]: " + message->c_str() );
 }
 
-std::string log_level_to_string(LogLevel level) {
-  if ((level & INFO) != 0) {
+std::string log_level_to_string( LogLevel level ) {
+  if ( ( level & INFO ) != 0 ) {
     return "INFO";
   }
-  if ((level & DEBUG) != 0) {
+  if ( ( level & DEBUG ) != 0 ) {
     return "DEBUG";
   }
-  if ((level & WARNING) != 0) {
+  if ( ( level & WARNING ) != 0 ) {
     return "WARNING";
   }
-  if ((level & ERROR) != 0) {
+  if ( ( level & ERROR ) != 0 ) {
     return "ERROR";
   }
-  if ((level & CRITICAL) != 0) {
+  if ( ( level & CRITICAL ) != 0 ) {
     return "CRITICAL";
   }
   return "UNKNOWN";
@@ -34,12 +35,18 @@ std::string log_level_to_string(LogLevel level) {
 
 // Replace this when we have a proper clock
 std::string get_time_as_string() {
+  std::string time_string;
+
   time_t current_time = std::chrono::system_clock::to_time_t(
-      std::chrono::high_resolution_clock::now());
-  std::string time_string = ctime(&current_time);
-  time_string = time_string.substr(0, time_string.length() - 1);
-  return time_string;
+      std::chrono::high_resolution_clock::now() );
+
+  std::time_t now = std::time( &current_time );
+  std::tm *ptm = std::localtime( &now );
+  char buffer[32];
+  // Format: 15.06.2009 20:20:00
+  std::strftime( buffer, 32, "%d-%m-%Y %H:%M:%S", ptm );
+  return buffer;
 }
-} // namespace Logger
-} // namespace Core
-} // namespace Thumpy
+}  // namespace Logger
+}  // namespace Core
+}  // namespace Thumpy

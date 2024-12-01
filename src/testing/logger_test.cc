7ddea3b5ae68c7aa872dda39e732adcdc5374883
Thumpy <KaliRaines@protonmail.com>
2024-12-01 11:49:57 -0700

    update ctest path

--- src/testing/logger_test.cc
+++ src/testing/logger_test.cc
@@ -1,50 +1,52 @@
 
-#include "file_logger.hpp"
-#include "logger_helper.hpp"
-#include "term_logger.hpp"
-#include <fstream>
 #include <gtest/gtest.h>
+
+#include <fstream>
 #include <ios>
 #include <logger.hpp>
 #include <string>
 
+#include "file_logger.hpp"
+#include "logger_helper.hpp"
+#include "term_logger.hpp"
+
 namespace Thumpy {
 namespace Core {
 // Demonstrate some basic assertions.
-TEST(engine_unit_test, test_file_logger_open) {
+TEST( engine_unit_test, test_file_logger_open ) {
   // Create file
   Logger::init();
-  EXPECT_TRUE(Logger::get_log_file()->is_open());
+  EXPECT_TRUE( Logger::get_log_file()->is_open() );
 
   // Close file
   Logger::close_log_file();
-  EXPECT_FALSE(Logger::get_log_file()->is_open());
+  EXPECT_FALSE( Logger::get_log_file()->is_open() );
 }
 
-TEST(engine_unit_test, test_file_logger_write) {
+TEST( engine_unit_test, test_file_logger_write ) {
   std::string test_string = "test_message";
 
   // Create file
   Logger::init();
-  EXPECT_TRUE(Logger::get_log_file()->is_open());
+  EXPECT_TRUE( Logger::get_log_file()->is_open() );
   // Log to file
-  Logger::log_to_file(test_string, Logger::LogLevel::DEBUG);
+  Logger::log_to_file( test_string, Logger::LogLevel::DEBUG );
 
   // Close file
   Logger::close_log_file();
-  EXPECT_FALSE(Logger::get_log_file()->is_open());
+  EXPECT_FALSE( Logger::get_log_file()->is_open() );
 
   // Open File
-  std::ifstream file_stream(Logger::log_path, std::ios::in);
-  EXPECT_TRUE(file_stream.is_open());
+  std::ifstream file_stream( Logger::log_path, std::ios::in );
+  EXPECT_TRUE( file_stream.is_open() );
 
   // Read file
   std::string line;
-  std::getline(file_stream, line);
+  std::getline( file_stream, line );
 
   // Validate equal
-  EXPECT_EQ(line, test_string);
+  EXPECT_EQ( line, test_string );
 }
-} // namespace Core
+}  // namespace Core
 
-} // namespace Thumpy
+}  // namespace Thumpy

c51e3568e8af8b9f266acd6846fd2ad5aaa5abe0
Thumpy <KaliRaines@protonmail.com>
2024-12-03 07:01:51 -0700

    added comments

--- src/assets/textures/migrate_textures.cmake
+++ src/assets/textures/migrate_textures.cmake
@@ -1,3 +1,4 @@
+
 message("Copying textures...")
 set(srcDir ${CMAKE_CURRENT_LIST_DIR})
 set(destDir ${CMAKE_CURRENT_BINARY_DIR}/assets/textures)
@@ -5,17 +6,16 @@ set(destDir ${CMAKE_CURRENT_BINARY_DIR}/assets/textures)
 message("Coping textures from: ${srcDir}")
 message("Coping textures to: ${destDir}")
 make_directory(${destDir})
-
-# Get all files with the following extentions
-# png
-# jpg
 file( GLOB_RECURSE  textures "${srcDir}/*.png" "${srcDir}/*.jpg")
 
 foreach(texture ${textures})
-    file(RELATIVE_PATH relative_path ${srcDir} ${texture})
+
+file(RELATIVE_PATH relative_path ${srcDir} ${texture})
+
     message("Copying texture: ${texture}")
     message("Copying to: ${destDir}/${relative_path}")
     configure_file(${texture} "${destDir}/${relative_path}" COPYONLY)
+
 endforeach(texture ${textures})
 
 
